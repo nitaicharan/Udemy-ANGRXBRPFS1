485c39cdfe020449ab799a4e9ca05f0e8eff0c7f
Nitai Charan <nitaicharan@gmail.com>
2020-05-29 00:00:00 -0300

    9. Installing Redux Devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AuthModule } from 'src/app/auth/auth.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { AuthModule } from 'src/app/auth/auth.module'
-import { StoreModule } from '@ngrx/store'
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +15,10 @@ import { StoreModule } from '@ngrx/store'
     AppRoutingModule,
     AuthModule,
     StoreModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
