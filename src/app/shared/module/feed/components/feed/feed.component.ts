26cc0e0dfa8e1601d9dd8c72cd7ba862807c76cc
Nitai Charan <nitaicharan@gmail.com>
2020-06-05 00:00:00 -0300

    28. Creating pagination

--- src/app/shared/module/feed/components/feed/feed.component.ts
+++ src/app/shared/module/feed/components/feed/feed.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { BackendErrors } from 'src/app/shared/model/backend-errors.model';
 import { FeedResponse } from '../../models/feed-response.model';
 import { feedAction } from '../../store/feed.action';
@@ -11,13 +12,20 @@ import { errorSelector, feedSelector, isLoadingSelector } from '../../store/feed
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss']
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnDestroy {
   @Input() url: string;
   feed$: Observable<FeedResponse>;
   error$: Observable<BackendErrors>;
   isLoading$: Observable<boolean>;
+  limit: number;
+  baseUrl: string;
+  queryParamsSubscription: Subscription;
+  currentPage: number;
 
-  constructor(private store: Store) {  }
+  constructor(
+    private store: Store,
+    private activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
     this.store.dispatch(feedAction({ url: this.url }));
@@ -25,5 +33,16 @@ export class FeedComponent implements OnInit {
     this.feed$ = this.store.select(feedSelector);
     this.error$ = this.store.select(errorSelector);
     this.isLoading$ = this.store.select(isLoadingSelector);
+
+    this.activatedRoute.queryParams.subscribe(
+      (params: Params) => {
+        const page = Number(params.page || '1');
+        this.currentPage = page;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.queryParamsSubscription.unsubscribe();
   }
 }
